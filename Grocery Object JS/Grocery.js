2266cf19a5d54f6f3b77137167d6ea95ec7c2a84
Michael Bray <mbray369@gmail.com>
2018-11-29 10:27:20 -0700

    DPE assignment complete

---
+++ Grocery Object JS/Grocery.js
@@ -0,0 +1,7 @@
+var shopper = {
+    name: "Swanson",
+    money: 100,
+    isUsingCash: true,
+    groceryCart: [eggs = 2.25, bacon = 7.25, onions = 4.00, carrots = 5.25, cabbage = 2.25, apples = 5.00, oranges = 5.00, bread = 5.50, mayo = 6.25, trailmix = 4.25, cheesecake = 6.00, beer  = 12.50]
+    function: 
+}
