18d03c3f586b1d71d6f60f3a9ae952e4c008a3a6
Michael Bray <mbray369@gmail.com>
2018-11-29 19:22:15 -0700

    Updating assignments

---
+++ Even-Odd-Loop/app.js
@@ -0,0 +1,7 @@
+//WHILE\\
+
+var result;
+
+while(result = Math.random() < .5){
+    console.log("You Win!")
+}
--- if-statement-olympics/app.js
+++ if-statement-olympics/app.js
@@ -1,31 +1,34 @@
-// if(5 > 3) {
-//     console.log("is greater than")
-// }
-// if("cat".length === 3) {
-//     console.log("is the length")
-// }
-// if("cat" === "dog") {}
-//     else {
-//         console.log("not the same")
-//     }
+if(5 > 3) {
+    console.log("is greater than")
+}
+if("cat".length === 3) {
+    console.log("is the length")
+}
+if("cat" === "dog") {
+    } else {
+        console.log("not the same")
+    }
 
-// for(var i = 0; i < 10; i++) {
-//     console.log(i)
-// }
-
-//WHILE\\
-
-// var reslult;
-
-// while(result = Math.random() < .5){
-//     console.log("You Win!")
-// }
+for(var i = 0; i < 10; i++) {
+    console.log(i)
+}
 
 for(var i = 0; i < 101; i++) {
     if(i % 2 === 0) {
         console.log("Even")
-    }
-    else {
+    } else {
         console.log("Odd")
     }
 }
+
+// BRONZE \\
+
+var person = {
+    name: "Bobby",
+    age: 12
+  };
+if(person.name[0] === "B" && person.age >= 18) {
+      console.log("You're Old Enough To Watch")
+} else {
+    console.log("Not Old Enough To Watch, Later Dawg..")
+};
