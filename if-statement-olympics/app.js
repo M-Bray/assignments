9320cc308762fb3cd57657127091a470ab90b145
Michael Bray <mbray369@gmail.com>
2018-11-29 20:33:06 -0700

    Finished up if-statement-olympics-silver

--- if-statement-olympics/app.js
+++ if-statement-olympics/app.js
@@ -32,3 +32,16 @@ if(person.name[0] === "B" && person.age >= 18) {
 } else {
     console.log("Not Old Enough To Watch, Later Dawg..")
 };
+
+// SILVER \\
+
+if(1 === "1") {
+    console.log("strict")
+} else if(1 === "1") {
+    console.log("loose")
+} else {
+    console.log("not equal at all")
+}
+if(1 <= 2 && 2 === 4 || (3*4) > 10 && (5 + 10) > 10) {
+    console.log("yes")
+}
