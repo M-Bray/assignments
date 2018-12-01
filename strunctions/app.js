efe2deac84337899f3ca33463718d492dbc31cf7
Michael Bray <mbray369@gmail.com>
2018-12-01 14:32:46 -0700

    strunctions-completion

--- strunctions/app.js
+++ strunctions/app.js
@@ -1,4 +1,4 @@
-//UPPER CASE\\
+//UPPERCASE\\
 var phrase = "Keep Moving";
 var upperPhrase = phrase.toUpperCase();
 console.log(upperPhrase); 
@@ -15,8 +15,8 @@ var fullStr = wrd.concat(wrd2);
 console.log(fullStr); 
 
 //INDEXoF\\
-var stringy = "Sitting on The Dock of The Bay"
-var nIndex = stringy.indexOf("n");
+var stringer = "Sitting on The Dock of The Bay"
+var nIndex = stringer.indexOf("n");
 console.log(nIndex);
 
 //LASTiNDEXoF\\
@@ -30,8 +30,24 @@ var arr = str.split(". ");
 console.log(arr);
 
 //SLICE\\
-var stry = 'Welcome To The New World';
+var stry = "Welcome To The New World";
 var newStry = stry.slice(8, 19);
 console.log(newStry); 
 
 //MATCH\\
+var str3 = "Obviously, I'm curiously coding constantly. continually, and carefully finding ways collectively "; 
+var res3 = str3.match(/ly/g);
+console.log(res3);
+
+//REPLACE\\
+
+var str4 = "All You Need Is Hate" 
+var res4 = str4.replace("Hate", "Love");
+console.log(res4);
+
+
+//SUBSTR\\
+
+var str5 = "This Garbage Method Is Being Phased Out";
+var res5 = str5.substr(4, 9);
+console.log(res5);
