8737cec32728febcac0ebb8c79321b740c828bb3
Michael Bray <mbray369@gmail.com>
2018-12-09 03:58:26 -0700

    update pest control

--- Mario-Pest-Control/app.js
+++ Mario-Pest-Control/app.js
@@ -1,9 +1,9 @@
-var kritter = document.getElementById("kritterInput");
 var zinger = document.getElementById("zingerInput");
+var kritter = document.getElementById("kritterInput");
 var miniNecky = document.getElementById("miniNeckyInput");
 var total = document.getElementById("totalButton");
 
 total.addEventListener("click", function(e){
     var pay = (+kritter.value * 5) + (+zinger.value * 7) + (+miniNecky.value * 11);
-    document.getElementById(payBox).innerHTML = ("Donkey Kong has earned a total of ") + pay + (" bananas");
+    document.getElementById("payBox").innerHTML = ("Donkey Kong has earned a total of ") + pay + (" bananas");
 })
