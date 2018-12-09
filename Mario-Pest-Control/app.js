ba53e0299829ea2012f17f4edeb1867f30a60cef
Michael Bray <mbray369@gmail.com>
2018-12-08 23:40:55 -0700

    update DK Pest Control

---
+++ Mario-Pest-Control/app.js
@@ -0,0 +1,9 @@
+var kritter = document.getElementById("kritterInput");
+var zinger = document.getElementById("zingerInput");
+var miniNecky = document.getElementById("miniNeckyInput");
+var total = document.getElementById("totalButton");
+
+total.addEventListener("click", function(e){
+    var pay = (+kritter.value * 5) + (+zinger.value * 7) + (+miniNecky.value * 11);
+    document.getElementById(payBox).innerHTML = ("Donkey Kong has earned a total of ") + pay + (" bananas");
+})
