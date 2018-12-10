832a110a1af706bfbcf25a7bb79e9d368a7ced6b
Michael Bray <mbray369@gmail.com>
2018-12-09 17:54:10 -0700

    social.js Final

---
+++ social.js/app.js
@@ -0,0 +1,34 @@
+var singer = {
+    name: "Mark",
+    age: 30,
+    favoriteSong: [
+        {
+            title: "My Girl",
+            artist: [
+                {
+                    band: "The Temptations",
+                    memberCount: 5,
+                    biggestHit: "Papa was a Rollin Stone",
+                }
+            ]
+        }
+    ],
+    singerFriends: ["Paul", 
+        {
+            name: "Mikey",
+            age: 26,
+            favoriteSong: [
+                {
+                    title: "Squash all Beef",
+                    artist: [
+                        {
+                            band: "KRS-One",
+                            memberCount: 1,
+                            biggestHit: "Sound of da Police",
+                        }
+                    ]
+                }
+            ]
+        }]
+}
+console.log(singer)
