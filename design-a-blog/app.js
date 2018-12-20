ed1793a081676fadd5f0e2b56a09feaef705d983
Michael Bray <mbray369@gmail.com>
2018-12-20 11:55:17 -0700

    practice grid on Travel-Form

--- design-a-blog/app.js
+++ design-a-blog/app.js
@@ -1,31 +1,39 @@
 
-var t = myWorld();
+// var t = myWorld();
 
 
-function myWorld() {
-    return(
-        "whatever it dont matter I guess"
-    )
-}
+// function myWorld() {
+//     return(
+//         "whatever it dont matter I guess"
+//     )
+// }
 
-function MultiplyByLength(arr) {
-	var newArr = [];
-	for (let i = 0; i < arr.length; i++) {
-		newArr.push(arr[i] * arr.length)
-	}
-	return newArr;
-}
-
-
-            ////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\             
+// function MultiplyByLength(arr) {
+// 	var newArr = [];
+// 	for (let i = 0; i < arr.length; i++) {
+// 		newArr.push(arr[i] * arr.length)
+// 	}
+// 	return newArr;
+// }
 
 
-function MultiplyByLength(arr) {
-	return arr.map( function(num) {
-		return num * arr.length;
-	})
-}
+//             ////////////////////////////\\\\\\\\\\\\\\\\\\\\\\\\\\\             
 
-function MultiplyByLength(arr) {
-	return arr.map( (num) => num * arr.length);
-}
+
+// function MultiplyByLength(arr) {
+// 	return arr.map( function(num) {
+// 		return num * arr.length;
+// 	})
+// }
+
+// function MultiplyByLength(arr) {
+// 	return arr.map( (num) => num * arr.length);
+// }
+
+function atTime(ms){
+	setTimeout(() => {
+		const date = new Date();
+		console.log(date.toLocaleString())
+	}, ms);
+} 
+atTime(5000)
