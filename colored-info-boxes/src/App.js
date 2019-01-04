2c79f12ba42c13a7ad89c42249ab0f9f2dea0989
Michael Bray <mbray369@gmail.com>
2019-01-03 22:14:20 -0700

    Color-info-boxes* learned styling props in React :)* hecka random color Gen

--- colored-info-boxes/src/App.js
+++ colored-info-boxes/src/App.js
@@ -4,7 +4,83 @@ import Boxy from "./Boxy"
 function App() {
   return (
     <div>
-      <Boxy />
+      <Boxy 
+        jobOpening={{job: "Construction", 
+        location: "Sacramento, CA.", 
+        summary: "Heavy Machinery Operator Needed ASAP", 
+        phone: "(916) 669-2125", 
+        email: "sacconstruction@work.com"}}
+      />
+      <Boxy 
+        jobOpening={{job: "Pool Cleaner", 
+        location: "Roseville, CA.", 
+        summary: "Looking for someone to clean my pool", 
+        phone: "(916) 369-3455", 
+        email: "someone@something.com"}} 
+      />
+      <Boxy 
+        jobOpening={{job: "Dishwasher", 
+        location: "Rocklin, CA.", 
+        summary: "Need someone in the dishpit", 
+        phone: "(916) 112-2345", 
+        email: "dirtykitchen@clean.com"}}
+        />
+      <Boxy 
+        jobOpening={{job: "Work", 
+        location: "Sacramento, CA.", 
+        summary: "Heavy Machinery Operator Needed ASAP", 
+        phone: "(916) 669-2125", 
+        email: "sacconstruction@work.com"}}
+        />
+      <Boxy 
+        jobOpening={{job: "Construction", 
+        location: "Sacramento, CA.", 
+        summary: "Heavy Machinery Operator Needed ASAP", 
+        phone: "(916) 669-2125", 
+        email: "sacconstruction@work.com"}}
+        />
+      <Boxy 
+        jobOpening={{job: "Construction", 
+        location: "Sacramento, CA.", 
+        summary: "Heavy Machinery Operator Needed ASAP", 
+        phone: "(916) 669-2125", 
+        email: "sacconstruction@work.com"}}
+        />
+      <Boxy 
+        jobOpening={{job: "Construction", 
+        location: "Sacramento, CA.", 
+        summary: "Heavy Machinery Operator Needed ASAP", 
+        phone: "(916) 669-2125", 
+        email: "sacconstruction@work.com"}}
+        />
+      <Boxy 
+        jobOpening={{job: "Construction", 
+        location: "Sacramento, CA.", 
+        summary: "Heavy Machinery Operator Needed ASAP", 
+        phone: "(916) 669-2125", 
+        email: "sacconstruction@work.com"}}
+        />
+      <Boxy 
+        jobOpening={{job: "Construction", 
+        location: "Sacramento, CA.", 
+        summary: "Heavy Machinery Operator Needed ASAP", 
+        phone: "(916) 669-2125", 
+        email: "sacconstruction@work.com"}}
+        />
+      <Boxy 
+        jobOpening={{job: "Construction", 
+        location: "Sacramento, CA.", 
+        summary: "Heavy Machinery Operator Needed ASAP", 
+        phone: "(916) 669-2125", 
+        email: "sacconstruction@work.com"}}
+        />
+      <Boxy 
+        jobOpening={{job: "Construction", 
+        location: "Sacramento, CA.", 
+        summary: "Heavy Machinery Operator Needed ASAP", 
+        phone: "(916) 669-2125", 
+        email: "sacconstruction@work.com"}}
+        />
     </div>
   )
 }
--- colored-info-boxes/src/Boxy.js
+++ colored-info-boxes/src/Boxy.js
@@ -1,11 +1,53 @@
 import React from "react"
 
-const Boxy = () => {
+const RandomColorGen = () => {
+  const colors = ["#86C2F0", "#FFC300", "#DAF7A6", "#BDF086", "#86F0AC", "#CE86F0", "magenta", "#F086D1", "#F08686"];
+  return colors[Math.floor(Math.random() * colors.length)]
+}
+const RandomColorGenB = () => {
+  const colorsB = ["#850202", "#BE5500", "#018E00", "#00858E", "#00048E", "#3E008E", "#6B008E", "#8E0071", "#8E0000"];
+  return colorsB[Math.floor(Math.random() * colorsB.length)]
+}
+const RandomColorGenC = () => {
+  const colorsC = ["#850202", "#BE5500", "#018E00", "#00858E", "#00048E", "#3E008E", "#6B008E", "#8E0071", "#8E0000"];
+  return colorsC[Math.floor(Math.random() * colorsC.length)]
+}
+const RandomColorGenD = () => {
+  const colorsD = ["#850202", "#BE5500", "#018E00", "#00858E", "#00048E", "#3E008E", "#6B008E", "#8E0071", "#8E0000"];
+  return colorsD[Math.floor(Math.random() * colorsD.length)]
+}
+const Boxy = (props) => {
+  let color = RandomColorGen()
+  let colorB =RandomColorGenB()
+  let colorC =RandomColorGenC()
+  let colorD =RandomColorGenD()
   return ( 
-    <div className="boxA">
-      <h1>Hello</h1>
-      <h3>Welcome</h3>
-      <p>Planet Earth Invites You!</p>
+    <div className="boxA" 
+    style={{
+      display: "flex",
+      backgroundColor: color,
+      alignItems: "center", 
+      border: "1px solid black",
+      borderRadius: "2px",
+      margin: "5px"
+      }}>
+      <h1 style={{
+        margin: "auto",
+        color: colorB
+      }}>{props.jobOpening.job}
+      </h1>
+      <h3 style={{
+        margin: "auto",
+        color: colorC
+      }}>{props.jobOpening.location}
+      </h3>
+      <p style={{
+        margin: "auto",
+        color: colorD
+      }}>{props.jobOpening.summary}
+      </p>
+      <footer>{props.jobOpening.phone}</footer>
+      <footer>{props.jobOpening.email}</footer>
     </div>
   )
 }
