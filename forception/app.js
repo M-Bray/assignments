8dac3ccd70cbe9e9e24760abe4dda1d3e2c6bba6
Michael Bray <mbray369@gmail.com>
2019-01-12 20:08:37 -0700

    made design-a-blog responsive and styled it nicely :)

---
+++ forception/app.js
@@ -0,0 +1,18 @@
+var people = ["Jon", "Jacob", "Jingle", "Heimer", "Schmidt"];
+var alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+// function forception(people) {
+//   for (var i = 0; i < people.length; i++) {
+//     console.log(people[i]);
+//   }
+// }
+// forception();
+
+
+function MultiplyByLength(arr) {
+  var i;
+  (i = 0; i <= arr.length; i++) {
+	console.log(people[i])
+ }
+}
+//1 (801) 703-6094‬
--- loops-exercise/app.js
+++ loops-exercise/app.js
@@ -1,6 +1,14 @@
-var str = "Hello My Name is Mikey"
-var arr = []
-for (var i = 0; i <= str.length; i++) {
-  arr.push[i]
-  console.log(arr)
+// var str = "Hello My Name is Mikey"
+// var arr = []
+// for (var i = 0; i <= str.length; i++) {
+//   arr.push[i]
+//   console.log(arr)
+// }
+
+
+
+function addUp(num) {
+	var n = parseInt(num);
+	return (n * (n + 1)) / 2;
 }
+console.log(addUp(3))
