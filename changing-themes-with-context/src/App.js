15f2aa3dfc13365e5871f8c6c8f848e197c9f573
Michael Bray <mbray369@gmail.com>
2019-01-14 17:57:03 -0700

    tried creating pixel art.. yikes..

---
+++ changing-themes-with-context/src/App.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react'
+import Divi from "./Divi"
+
+export default class App extends Component {
+  render() {
+    return (
+      <Divi />  
+    )
+  }
+}
---
+++ changing-themes-with-context/src/Divi.js
@@ -0,0 +1,123 @@
+import React, { Component } from 'react'
+
+export default class Divi extends Component {
+  render() {
+    return (
+      <div className="diviWrap">
+        <div className="black"></div>
+        <div className="black"></div>
+        <div className="black"></div>
+        <div className="black"></div>
+        <div className="black"></div>
+        <div className="brown A"></div>
+        <div className="brown B"></div>
+        <div className="black"></div>
+        <div className="black"></div>
+        <div className="black"></div>
+
+        <div  class="black"></div>
+        <div  class="black"></div>
+        <div  class="black"></div>
+        <div class="brown A"></div>
+        <div class="brown"></div>
+        <div class="brown"></div>
+        <div class="brown"></div>
+        <div class="brown B"></div>
+        <div class="black"></div>
+        <div class="black"></div>
+        
+        <div class="black"></div>
+        <div class="black"></div>
+        <div class="brown A"></div>
+        <div class="brown"></div>
+        <div class="tan A">
+          <div class=" A"></div>
+        </div>
+          
+        <div class="tan"></div>
+        <div class="tan B "></div>
+        <div class="black "></div>
+        <div class="black"></div>
+        <div class="black"></div>
+
+
+        <div class="black"></div>
+        <div class="brown A"></div>
+        <div class="brown"></div>
+        <div class="tan A"></div>
+        <div class="tan"></div>
+        <div class="blue"></div>
+        <div class="tan"></div>
+        <div class="blue B C"></div>
+        <div class="black"></div>
+        <div class="black"></div>
+
+        <div className="black"></div>
+        <div className="brown"></div>
+        <div className="brown"></div>
+        <div className="tan"></div>
+        <div className="tan"></div>
+        <div className="tan"></div>
+        <div className="tan"></div>
+        <div></div>
+        <div></div>
+        <div></div>
+
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    )
+  }
+}
