8c688d71fda53865467a34fb06dd1486c8f47c3e
Michael Bray <mbray369@gmail.com>
2019-01-20 18:13:51 -0700

    trying to add a background image, and did some more styling

--- text-detect/client/src/Header.js
+++ text-detect/client/src/Header.js
@@ -4,7 +4,7 @@ import React from 'react'
 const Header = () => {
   return (
     <header className="headerClass">
-      <h1 className="titleHeader">Text Detect</h1>     
+      <h1 className="titleHeader">Text Detect</h1>
       <h3 className="welcomeHeader">A Picture Says a Thousand Words</h3>
       <p className="introPar">
         Here you can identify and extract text from within an image.
