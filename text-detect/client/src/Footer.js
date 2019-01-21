e692fb20fc5e1b3ed757339179be4387d12d9ba4
Michael Bray <mbray369@gmail.com>
2019-01-21 14:08:10 -0700

    added handwritten note and showing the text extracted from the image

--- text-detect/client/src/Footer.js
+++ text-detect/client/src/Footer.js
@@ -4,7 +4,6 @@ import React from 'react'
 const Footer = () => {
   return(
     <div className="footerDiv">
-      <h3 className="">We hope to have been of service.</h3>
       <p className="">Please tell how we might improve your experience here at TextDetect.</p>
       <footer className="">Copyright © 2019 Michael Bray. All Rights Reserved.</footer>
     </div>
--- text-detect/client/src/VisionData.js
+++ text-detect/client/src/VisionData.js
@@ -24,7 +24,9 @@ export default class VisionData extends Component {
   render() {
     return (
       <div className="dataDiv">
+        <img className="imgExample" src='https://media.npr.org/assets/img/2016/04/17/handwritten-note_wide-941ca37f3638dca912c8b9efda05ee9fefbf3147.jpg?s=1400' />
         <p className="dataPar">{this.state.text}</p>
+
       </div>
     )
   }
