558aaa376ed7cc08c97217de7ab6ebb4324af508
Michael Bray <mbray369@gmail.com>
2019-01-23 18:21:09 -0700

    addiong content to quality page / component

--- text-detect/client/src/Quality.js
+++ text-detect/client/src/Quality.js
@@ -4,10 +4,24 @@ import React from 'react'
 const Quality = () => {
   return (
     <div className="qualityDiv">
-      <h3>Our Team</h3>
-      <p>Our team consists of a </p>
-      <h2>Our Service</h2>
-      <h1>Our Customer</h1>
+      <div>
+        <h3>Our Team</h3>
+        <p>Our team consists of a </p>
+      </div>
+      <div>
+        <h2>We provide a service that allows you to extract and transcribe text from images.</h2>
+        <ul></ul>
+          <li>hand-written notes</li>
+          <li>Documents
+            <li>Images of documents can now be easily transcribed into text</li>
+          </li>
+        <ul>Mood Detection</ul>
+        <ul></ul>
+      </div>
+      <div>
+        <h1>Our Customer</h1>
+      </div>
+      
     </div>
   )
 }
