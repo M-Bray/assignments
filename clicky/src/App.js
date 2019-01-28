c6eee821d06b665ab711b495d733386182170d2e
Michael Bray <mbray369@gmail.com>
2019-01-27 19:33:56 -0700

    click count

---
+++ clicky/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default class App extends React.Component { 
+  constructor(){
+    super()
+    this.state = {
+      count: 0
+    }
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    this.setState(prevState => {
+      return {
+        count: prevState.count + 1
+      }
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <hi>{this.state.count}</hi>
+        <button onClick={this.handleClick}>Click</button>
+      </div>
+    )
+  }
+}
+
