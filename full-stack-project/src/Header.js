19f751e87fd09e37105c3d4bc7e6f8d27dbe12cb
Michael Bray <mbray369@gmail.com>
2019-01-30 17:27:51 -0700

    updated full stack-project

--- full-stack-project/src/Header.js
+++ full-stack-project/src/Header.js
@@ -3,12 +3,12 @@ import React from 'react'
 function Header() {
   return (
     <div className="header-wrap">
-    <h1 className="title-header">Photo Bomb</h1>
-      <h4 className="welcome-header"><i>A Picture Says a Thousand Words</i></h4>
-      <p className="intro-par">
+      <h1 className="title-header" id="no-filter">Photo Bomb</h1>
+      <h4 className="welcome-header" id="no-filter"><i>A Picture Says a Thousand Words</i></h4>
+      <p className="intro-par" id="no-filter">
         Here you can reveal hidden secrets from within an image.
       </p>
-      <h2 className="header-exit">Give it a Go!</h2>
+      <h2 className="header-exit" id="no-filter">Give it a Go!</h2>
     </div>
   )
 }
--- full-stack-project/src/Nav.js
+++ full-stack-project/src/Nav.js
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 function Nav(props) {
   return (
     <nav className="nav-tab">
-      <Link to="/" className="nav-buttons" ><b>H</b>ome</Link>
-      <Link to="/help" className="nav-buttons" ><b>H</b>elp</Link>
-      <Link to="/form" className="nav-buttons"><b>F</b>orm</Link>
+      <Link to="/" className="nav-buttons" >Home</Link>
+      <Link to="/help" className="nav-buttons" >Help</Link>
+      <Link to="/form" className="nav-buttons">Form</Link>
     </nav>
   )
 }
