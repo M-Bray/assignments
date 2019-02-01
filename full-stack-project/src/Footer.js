ad1a5e13955c0a3edf72c1f629a54ee3b57d0d39
Michael Bray <mbray369@gmail.com>
2019-01-31 23:37:03 -0700

    finding the right style

--- full-stack-project/src/Footer.js
+++ full-stack-project/src/Footer.js
@@ -4,7 +4,6 @@ import React from 'react'
 const Footer = () => {
   return(
     <div className="footer-div">
-      <p className="footer-par">Please tell us how we might improve your experience here at TextDetect.</p>
       <footer className="footer-copyright">Copyright © 2019 Michael Bray. All Rights Reserved.</footer>
     </div>
   )
