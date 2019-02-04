a4cf9fbf817837b06b74f908c82bd7e1d3bc5385
Michael Bray <mbray369@gmail.com>
2019-02-04 00:27:29 -0700

    app is operational. needs styles

--- text-detect/client/src/VisionData.js
+++ text-detect/client/src/VisionData.js
@@ -29,7 +29,9 @@ export default class VisionData extends Component {
       axios.get("/labels")
       .then(response => {
         console.log(response.data[0].labelAnnotations)
-        this.setState({ labels: response.data[0].labelAnnotations, hasLoaded: true })
+        const labels = response.data[0].labelAnnotations
+        const hasLoaded = true
+        this.setState({ labels, hasLoaded })
       })
 }
 
@@ -37,7 +39,15 @@ export default class VisionData extends Component {
     return (
       <div className="data-div">
         <img onClick={this.handleLabelClick} className="img-example" src="https://media.npr.org/assets/img/2016/04/17/handwritten-note_wide-941ca37f3638dca912c8b9efda05ee9fefbf3147.jpg?s=1400" alt="handwritten note" />
-        {this.state.hasLoaded && <p className="data-par">{this.state.text}</p>}
+        {this.state.hasLoaded && <p className="data-par"></p>}
+        <div>{this.state.labels.map(label => {
+          return (
+            <div>
+              {label.description}
+            </div>
+          )
+          })}
+        </div>
 
       </div>
     )
