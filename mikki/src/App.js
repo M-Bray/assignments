25ed4793d997c4ae8b623f9d925fb9156368ada8
Michael Bray <mbray369@gmail.com>
2019-02-08 18:59:00 -0700

    created folder for Mikki's Restaurant

---
+++ mikki/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>This is the beggining of Mikki's Restaurant website</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
