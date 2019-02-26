58f9adabb015993f93661f768c93ad5a4c760310
Michael Bray <mbray369@gmail.com>
2019-02-25 22:50:23 -0700

    furthering the mobilization

--- text-detect/client/src/Quality.js
+++ text-detect/client/src/Quality.js
@@ -5,11 +5,9 @@ const Quality = () => {
   return (
     <div className="header-wrap">
       <div className="quality-div">
-        <h1>Quality</h1>
+        <h3>Quality</h3>
         <p>The intentions for this app are to keep things simple and straight to the point.</p>
-      </div>
-      <div className="improvement-div">
-        <h1>Feedback</h1>
+        <h3>Feedback</h3>
         <p>Please tell us what you think about Text-Detect and let us know how we may improve our services to better your experience, Thank You.</p>
       </div>
     </div>
