0575ecc7af7b1f263512ac1cef309d721eb8c8e9
Michael Bray <mbray369@gmail.com>
2019-09-01 23:06:35 -0600

    Delete app.js

--- practice/pokemonlist/app.js
+++
@@ -1,5 +0,0 @@
-//Promise
-var axios = require("axios");
-axios.get("https://api.vschool.io/pokemon")
-    .then(response => console.log(response.data))
-    .catch(err => console.log(" CODE:247\n Sorry notSorry()"));
