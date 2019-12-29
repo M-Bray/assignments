c7b168ceeea05f6c67bdd99f0c970affb726f10b
Michael Bray <mbray369@gmail.com>
2019-12-29 11:29:29 -0800

    checking out canvas.. ehh..

--- 12-26-2019/canvas.js
+++ 12-26-2019/canvas.js
@@ -1,27 +1,31 @@
 const canvas = document.getElementById("canvas");
-
+const ctx = canvas.getContext("2d");
 if (canvas.getContext) {
-    const ctx = canvas.getContext("2d");
-    for (let i = 10; i < 200; i += 20) {
+    for (let i = 0; i < 200; i += 10) {
         ctx.moveTo(0, i);
         ctx.lineTo(canvas.width, i);
+        console.log(canvas.width);
         ctx.strokeStyle = 'green';
         ctx.stroke();
     }
-    for (let i = 10; i <400; i += 20) {
+    ctx.beginPath();
+    for (let i = 0; i < 400; i += 10) {
       ctx.moveTo(i, 0);
       ctx.lineTo(i,canvas.width/2);
       ctx.strokeStyle = 'blue';
       ctx.stroke();
      }
 }
-    
-ctx.arc(150, 75, 20, 0, 2 * Math.PI, false); 
-ctx.fillStyle = "blue";
-ctx.fill();
 
-ctx.beginPath();
-ctx.moveTo(0, 0);
-ctx.lineTo(300, 500)
-ctx.strokeStyle = '#ff0000';
-ctx.stroke();
+const animation = document.getElementsByClassName("animate");
+animation ? animation : console.log(animation);
+    
+// ctx.arc(150, 75, 20, 0, 2 * Math.PI, false); 
+// ctx.fillStyle = "blue";
+// ctx.fill();
+
+// ctx.beginPath();
+// ctx.moveTo(0, 0);
+// ctx.lineTo(300, 500)
+// ctx.strokeStyle = '#ff0000';
+// ctx.stroke();
